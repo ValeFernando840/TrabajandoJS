2358e4f6890c76f175ef5d86a70e3a0b8a046c60
Alexis <vale.alexis18@gmail.com>
2022-04-06 23:10:25 -0300

    nuevas funciones en el login

--- practica/Login/login.js
+++ practica/Login/login.js
@@ -1,36 +1,51 @@
 //variables
+const usuarioLogin=document.getElementById("usuarioLogin");
+const passwordLogin=document.getElementById("passwordLogin");
+//const form_login=document.querySelector(".form_login");
 
-
-//
+//defino los campos and uso otro Admin
 const campoLogin={
   usuario:false,
   password:false,
+  admin:false,
 };
-
 function verificar(entrada){
-  console.log(entrada.name)
-  if(expressions.usuario.test(entrada.value)){
-    console.log("entrada Usuario Correcta");
-    campoLogin["usuario"]=true;
-  }else{
-    console.log("entrada Usuario Incorrecta");
-    campoLogin["usuario"]=false;
+  //console.log("vengo del Verificar", entrada.name)
+
+  if(entrada.id=="usuarioLogin"){
+    if(expressions.usuario.test(entrada.value)){
+   
+      campoLogin["usuario"]=true;
+    }else{
+      
+      campoLogin["usuario"]=false;
+    }
   }
-  if(expressions.password.test(entrada.value)){
-    console.log("entrada Password correcta");
-    campoLogin["password"]=true;
-  }else{
-    console.log("entrada Password incorrecta");
-    campoLogin["password"]=false;
-}
+  if(entrada.id=="passwordLogin"){
+    if(expressions.password.test(entrada.value)){
+     
+      campoLogin["password"]=true;
+    }else{
+      
+      campoLogin["password"]=false;
+    }
+  }
   console.log(campoLogin);
   habilitarBtnIniciar();
 }
+
 function habilitarBtnIniciar(){
   if(campoLogin.usuario && campoLogin.password){
     document.querySelector(".form_login button").disabled=false;
-    console.log("se desactivo el Disabled");
+
   }else{
     document.querySelector(".form_login button").disable=true;
+    
   }
+}
+//definimos los listening
+form_login.addEventListener("submit",validarEntradas);
+
+function validarEntradas(){
+  
 }
